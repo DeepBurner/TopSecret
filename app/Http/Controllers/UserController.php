bc1ff5773609c472b1316f359f13760d35db4fd9
Ahmet Burak Çatlı <ahmetburakcatli@gmail.com>
2016-11-15 18:36:53 +0300

    login

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,8 +4,13 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller {
+
+    public function getDashboard(){
+        return view('dashboard');
+    }
 
     public function postSignUp(Request $request){
         $email = $request['email'];
@@ -18,12 +23,16 @@ class UserController extends Controller {
         $user->password = $password;
 
         $user->save();
+        Auth::login($user);
 
-        return redirect()->back();
+        return redirect()->route('dashboard');
 
     }
 
-    public function postSignIn(){
-
+    public function postSignIn(Request $request){
+        if (Auth::attempt(['email' => $request['email'], 'password' => $request['password']])){
+            return redirect() -> route('dashboard');
+        }
+        return redirect()->back();
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,16 +27,17 @@
         </div>
         <div class="col-md-6">
             <h3>Sign In</h3>
-            <form action="#" method="post">
+            <form action="{{ route('signin') }}" method="post">
                 <div class="form-group">
                     <label for="email">E-Mail</label>
                     <input class="form-control" type="text" name="email" id="email">
                 </div>
                 <div class="form-group">
-                    <label for="email">Password</label>
+                    <label for="password">Password</label>
                     <input class="form-control" type="password" name="password" id="password">
                 </div>
                 <button type="submit" class="btn btn-primary">Submit</button>
+                <input type="hidden" name="_token" value="{{ Session::token() }}">
             </form>
         </div>
     </div>
