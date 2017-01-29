86ac10d77b164b952dfb4c9b22f8603ffdff8805
Burak Künkçü <bkunkcu@gmail.com>
2017-01-29 12:59:01 +0300

    fix

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,8 +7,6 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
         <link rel="stylesheet" href="{{ URL::asset('css/main.css') }}">
-
-		
     </head>
     <body>
         @include('includes.header')
