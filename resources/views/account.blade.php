55427c59c8a46412e0ed97e308330ef96429a534
Ahmet Burak Çatlı <ahmetburakcatli@gmail.com>
2017-02-01 23:50:12 +0300

    small fix

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -32,32 +32,6 @@
         </div>
     </div>
 
-    <br>
-
-    <h1>Create a new message</h1>
-    {!! Form::open(['route' => 'messages.send']) !!}
-    <div class="col-md-6">
-        <!-- Subject Form Input -->
-
-        <div class="form-group">
-            {!! Form::label('receiver', 'Receiver', ['class' => 'control-label']) !!}
-            {!! Form::text('receiver', '{{ $user->id }}', ['class' => 'form-control']) !!}
-        </div>
-
-        <!-- Message Form Input -->
-        <div class="form-group">
-            {!! Form::label('message', 'Message', ['class' => 'control-label']) !!}
-            {!! Form::textarea('message', null, ['class' => 'form-control']) !!}
-        </div>
-
-
-    <!-- Submit Form Input -->
-        <div class="form-group">
-            {!! Form::submit('Submit', ['class' => 'btn btn-primary form-control']) !!}
-        </div>
-    </div>
-    {!! Form::close() !!}
-
 
     <!-- modal for send message screen  -->
     <div class="modal fade" tabindex="-1" role="dialog" id="edit-modal">
